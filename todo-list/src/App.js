e8319cad3e39bb671873a2a6599c6af712ba1b74
Pyreball <45050338+Pyreball@users.noreply.github.com>
2021-10-25 21:46:30 -0500

    Added ToDoForm which allows sers to add to items to the list.

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 //Components
 import Header from "./Header.js"
 import data from "./data.json"
-
+import ToDoList from "./ToDoList.js"
+import ToDoForm from "./ToDoForm.js"
 
 
 
@@ -13,11 +14,31 @@ function App() {
 
   const [ toDoList, setToDoList ] = useState(data);
 
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
+  }
+
+const handleFilter = () => {
+  let filtered = toDoList.filter(task => {
+    return !task.complete;
+  });
+  setToDoList(filtered);
+}
+
+const addTask = ( userInput ) => {
+  let copy = [...toDoList];
+  copy = [...copy, { id: toDoList.length + 1, task: userInput, complete:false }];
+  setToDoList(copy);
+}
 
   return (
     <div className="App">
       <Header />
-      <ToDoList toDoList={toDoList}/>
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+      <ToDoForm addTask={addTask}/>
     </div>
   );
 }
--- todo-list/src/ToDoList.js
+++ todo-list/src/ToDoList.js
@@ -1,1 +1,18 @@
-export default ToDoList
+import React from 'react'
+import Todo from "./ToDo"
+
+
+const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
+    return (
+        <div>
+            {toDoList.map(todo => {
+                return (
+                    <Todo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>)
+            })}
+            <button style={{margin: '20px'}} onClick={handleFilter}>Clear Completed</button>
+        </div>)
+}
+
+export default ToDoList;
+
+
