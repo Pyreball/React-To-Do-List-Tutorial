92818f1ae8b7a485978735d617732140f642321e
Patrick <patrick.a.terpstra@gmail.com>
2021-10-25 19:33:54 -0500

    commit todo and todolist

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,9 +1,19 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+
 //Components
 import Header from "./Header.js"
+import data from "./data.json"
+
+
+
 
 function App() {
+
+  const [ toDoList, setToDoList ] = useState(data);
+
+
   return (
     <div className="App">
       <Header />
--- todo-list/src/Header.js
+++ todo-list/src/Header.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Header = () => {
     return (
     <header>
-        <Header />
+        <h1>To Do List</h1>
     </header>
-    )
-}
+    );
+};
 
 export default Header;
